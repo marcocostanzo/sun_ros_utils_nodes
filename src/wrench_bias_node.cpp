ab6016c636f70a99a1a5fd78cce19e86cb98d314
Marco Costanzo <marco.costanzo@unicampania.it>
2020-02-22 18:47:32 +0100

    using ConstPtr and ros::topic::waitForMessage

--- src/wrench_bias_node.cpp
+++ src/wrench_bias_node.cpp
@@ -3,7 +3,7 @@
 
     Copyright 2019 Università della Campania Luigi Vanvitelli
 
-	Author: Marco Costanzo <marco.costanzo@unicampania.it>
+    Author: Marco Costanzo <marco.costanzo@unicampania.it>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,130 +19,128 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ros/ros.h"
 #include "geometry_msgs/WrenchStamped.h"
+#include "ros/ros.h"
 #include "std_srvs/Trigger.h"
 
 using namespace std;
 
-bool b_publish; //if can publish
-bool b_wrench_in_arrived; //if wrench is arrived
+bool can_publish;  // if can publish
 
 int samples_to_use_for_bias;
 
-geometry_msgs::WrenchStamped wrench_in;
+geometry_msgs::WrenchStamped::ConstPtr wrench_in;
 geometry_msgs::WrenchStamped wrench_biased;
 geometry_msgs::WrenchStamped wrench_bias;
 ros::Publisher pub_wrench_bias;
+ros::Subscriber sub_wrench;
 
-//User fnc
+// User fnc
 void compute_bias();
 void wait_wrench_measure();
 
-void read_wrench_cb( const geometry_msgs::WrenchStamped::ConstPtr& msg  ){
-
-    wrench_in = *msg;
-    b_wrench_in_arrived = true;
-
-    wrench_biased.header = wrench_in.header;
-
-    wrench_biased.wrench.force.x = wrench_in.wrench.force.x - wrench_bias.wrench.force.x;
-    wrench_biased.wrench.force.y = wrench_in.wrench.force.y - wrench_bias.wrench.force.y;
-    wrench_biased.wrench.force.z = wrench_in.wrench.force.z - wrench_bias.wrench.force.z;
-    wrench_biased.wrench.torque.x = wrench_in.wrench.torque.x - wrench_bias.wrench.torque.x;
-    wrench_biased.wrench.torque.y = wrench_in.wrench.torque.y - wrench_bias.wrench.torque.y;
-    wrench_biased.wrench.torque.z = wrench_in.wrench.torque.z - wrench_bias.wrench.torque.z;
-	
-    if(b_publish)
-        pub_wrench_bias.publish(wrench_biased);
-
-}
-
-bool compute_bias_service_cb(std_srvs::Trigger::Request &request,
-                                  std_srvs::Trigger::Response &response)
+void read_wrench_cb(const geometry_msgs::WrenchStamped::ConstPtr &msg)
 {
-    b_publish = false;
-    compute_bias();
-    b_publish = true;
-    response.message = "Bias Computed!";
-    response.success = true;
-    return true;
+  wrench_in = msg;
+
+  wrench_biased.header = wrench_in->header;
+
+  wrench_biased.wrench.force.x = wrench_in->wrench.force.x - wrench_bias.wrench.force.x;
+  wrench_biased.wrench.force.y = wrench_in->wrench.force.y - wrench_bias.wrench.force.y;
+  wrench_biased.wrench.force.z = wrench_in->wrench.force.z - wrench_bias.wrench.force.z;
+  wrench_biased.wrench.torque.x = wrench_in->wrench.torque.x - wrench_bias.wrench.torque.x;
+  wrench_biased.wrench.torque.y = wrench_in->wrench.torque.y - wrench_bias.wrench.torque.y;
+  wrench_biased.wrench.torque.z = wrench_in->wrench.torque.z - wrench_bias.wrench.torque.z;
+
+  if (can_publish)
+    pub_wrench_bias.publish(wrench_biased);
 }
 
+bool compute_bias_service_cb(std_srvs::Trigger::Request &request, std_srvs::Trigger::Response &response)
+{
+  can_publish = false;
+  compute_bias();
+  can_publish = true;
+  response.message = "Bias Computed!";
+  response.success = true;
+  return true;
+}
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc,argv,"wrench_bias");
+  ros::init(argc, argv, "wrench_bias");
 
-    ros::NodeHandle nh_private("~");
-    ros::NodeHandle nh_public;
+  ros::NodeHandle nh_private("~");
+  ros::NodeHandle nh_public;
 
-    /**** PARAMS ****/
-    string str_in_topic;
-    nh_private.param("in_topic" , str_in_topic, string("wrench_in") );
-    string str_out_topic;
-    nh_private.param("out_topic" , str_out_topic, str_in_topic + string("_biased") );
-    string str_bias_service;
-    nh_private.param("bias_service" , str_bias_service, string("compute_bias") );
-    nh_private.param("samples_to_use_for_bias" , samples_to_use_for_bias, 100 );
-    /************************************/
+  /**** PARAMS ****/
+  string str_in_topic;
+  nh_private.param("in_topic", str_in_topic, string("wrench_in"));
+  string str_out_topic;
+  nh_private.param("out_topic", str_out_topic, str_in_topic + string("_biased"));
+  string str_bias_service;
+  nh_private.param("bias_service", str_bias_service, string("compute_bias"));
+  nh_private.param("samples_to_use_for_bias", samples_to_use_for_bias, 100);
+  /************************************/
 
-    /*******INIT ROS PUB**********/
-	pub_wrench_bias = nh_public.advertise<geometry_msgs::WrenchStamped>( str_out_topic, 1);
-    /***************************/
+  /*******INIT ROS PUB**********/
+  pub_wrench_bias = nh_public.advertise<geometry_msgs::WrenchStamped>(str_out_topic, 1);
+  /***************************/
 
-    /*******INIT ROS SUB**********/
-	ros::Subscriber sub_wrench = nh_public.subscribe( str_in_topic , 1, read_wrench_cb);
-    /***************************/
+  /*******INIT ROS SUB**********/
+  sub_wrench = nh_public.subscribe(str_in_topic, 1, read_wrench_cb);
+  /***************************/
 
-    /*******INIT ROS SERVICES**********/
-    ros::ServiceServer srv_compute_bias = nh_public.advertiseService(str_bias_service, compute_bias_service_cb);
-    /***************************/
+  /*******INIT ROS SERVICES**********/
+  ros::ServiceServer srv_compute_bias = nh_public.advertiseService(str_bias_service, compute_bias_service_cb);
+  /***************************/
 
-    //Compute the first bias!
-    b_publish = false;
-    compute_bias();
-    b_publish = true;
+  // Compute the first bias!
+  can_publish = false;
+  compute_bias();
+  can_publish = true;
 
-    ros::spin();
+  ros::spin();
 
-    return 0;
+  return 0;
 }
 
 void compute_bias()
 {
-    ROS_INFO("wrench_bias: Compute Bias...");
-    //set bias to zero
-    wrench_bias.wrench.force.x = 0.0;
-    wrench_bias.wrench.force.y = 0.0;
-    wrench_bias.wrench.force.z = 0.0;
-    wrench_bias.wrench.torque.x = 0.0;
-    wrench_bias.wrench.torque.y = 0.0;
-    wrench_bias.wrench.torque.z = 0.0;
+  ROS_INFO("wrench_bias: Compute Bias...");
+  // set bias to zero
+  geometry_msgs::WrenchStamped wrench_bias_local;
+  wrench_bias_local.wrench.force.x = 0.0;
+  wrench_bias_local.wrench.force.y = 0.0;
+  wrench_bias_local.wrench.force.z = 0.0;
+  wrench_bias_local.wrench.torque.x = 0.0;
+  wrench_bias_local.wrench.torque.y = 0.0;
+  wrench_bias_local.wrench.torque.z = 0.0;
 
-    //Compute bias
-    for(int i=0; i<samples_to_use_for_bias; i++)
-    {
-        wait_wrench_measure();
-        wrench_bias.wrench.force.x += wrench_in.wrench.force.x;
-        wrench_bias.wrench.force.y += wrench_in.wrench.force.y;
-        wrench_bias.wrench.force.z += wrench_in.wrench.force.z;
-        wrench_bias.wrench.torque.x += wrench_in.wrench.torque.x;
-        wrench_bias.wrench.torque.y += wrench_in.wrench.torque.y;
-        wrench_bias.wrench.torque.z += wrench_in.wrench.torque.z;
-    }
-    wrench_bias.wrench.force.x /= samples_to_use_for_bias;
-    wrench_bias.wrench.force.y /= samples_to_use_for_bias;
-    wrench_bias.wrench.force.z /= samples_to_use_for_bias;
-    wrench_bias.wrench.torque.x /= samples_to_use_for_bias;
-    wrench_bias.wrench.torque.y /= samples_to_use_for_bias;
-    wrench_bias.wrench.torque.z /= samples_to_use_for_bias;
-    ROS_INFO("wrench_bias: Compute Bias DONE");
+  // Compute bias
+  for (int i = 0; i < samples_to_use_for_bias; i++)
+  {
+    wait_wrench_measure();
+    wrench_bias_local.wrench.force.x += wrench_in->wrench.force.x;
+    wrench_bias_local.wrench.force.y += wrench_in->wrench.force.y;
+    wrench_bias_local.wrench.force.z += wrench_in->wrench.force.z;
+    wrench_bias_local.wrench.torque.x += wrench_in->wrench.torque.x;
+    wrench_bias_local.wrench.torque.y += wrench_in->wrench.torque.y;
+    wrench_bias_local.wrench.torque.z += wrench_in->wrench.torque.z;
+  }
+  wrench_bias_local.wrench.force.x /= samples_to_use_for_bias;
+  wrench_bias_local.wrench.force.y /= samples_to_use_for_bias;
+  wrench_bias_local.wrench.force.z /= samples_to_use_for_bias;
+  wrench_bias_local.wrench.torque.x /= samples_to_use_for_bias;
+  wrench_bias_local.wrench.torque.y /= samples_to_use_for_bias;
+  wrench_bias_local.wrench.torque.z /= samples_to_use_for_bias;
+  wrench_bias_local.header.stamp = ros::Time::now();
+
+  wrench_bias = wrench_bias_local;
+  ROS_INFO("wrench_bias: Compute Bias DONE");
 }
 
 void wait_wrench_measure()
-{   
-    b_wrench_in_arrived = false;
-    while(ros::ok() && !b_wrench_in_arrived)
-        ros::spinOnce();
+{
+  wrench_in = ros::topic::waitForMessage<geometry_msgs::WrenchStamped>(sub_wrench.getTopic());
 }
